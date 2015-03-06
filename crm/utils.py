90cc7bb0f025a952d26d0457ad8ef1dd7e0f63d7
Carlos Killpack <carlos@zulumarketing.com>
2015-03-06 14:58:02 -0700

    Implemented navigation menus and improved groups interaction.

--- crm/utils.py
+++ crm/utils.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
 import re
-
-from django.conf import settings
 
 from .exceptions import NotAllowed, LoginRequired
 
@@ -13,7 +10,7 @@ def check_permissions(path, restrictions, request) -> bool:
     Enforces the rules specified in `CRM_CONTENT_RESTRICTIONS`.
     """
     for rule in restrictions:
-        if rule['path'] == '*' or re.match(r'^' + rule['path'] + r'.+$', path):
+        if rule['path'] == '*' or (path is not None and re.match(r'^' + rule['path'] + r'.+$', path)):
             pass
         else:
             continue
@@ -23,21 +20,18 @@ def check_permissions(path, restrictions, request) -> bool:
             raise LoginRequired()
 
 
-def root_exists(content_root) -> bool:
-    """
-    Checks if the content root actually exists.
-    """
-    if content_root is None:
-        return False
-    else:
-        return os.path.exists(os.path.join(settings.BASE_DIR, 'crm/templates/', content_root))
-
-
 def get_title(path) -> str:
     """
     Parses a filename and returns a formatted title.
     """
     name = path.split('/')[-1]
-    name = re.sub('--', ':', name)
+    name = re.sub('--', ': ', name)
     name = re.sub('-', ' ', name)
     return name.title()
+
+
+def get_slug(path) -> str:
+    """"""
+    path = 'index' if path is None else path
+    path = path[:-1] if path[-1] == '/' else path
+    return path
--- crm/views.py
+++ crm/views.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
+import re
 
 from django.conf import settings
 from django.views.generic import View
-from django.http import Http404, HttpResponseForbidden
+from django.http import Http404
 from django.shortcuts import render
 from django.template import TemplateDoesNotExist
 from django.contrib.auth.views import redirect_to_login
+from django.core.exceptions import PermissionDenied
 
-from .utils import root_exists, check_permissions, get_title
+from .utils import check_permissions, get_title, get_slug
 from .exceptions import NotAllowed, LoginRequired
 
 log = logging.getLogger('django.request')
@@ -23,6 +26,7 @@ class Content(View):
     restrictions = tuple()
 
     def get_content_root(self):
+        """Override this to provide dynamic content roots"""
         return self.content_root
 
     def get_restrictions(self) -> list:
@@ -36,16 +40,56 @@ class Content(View):
 
         Available access parameters are:
 
-        'to_group': Any group name.
+        'in_group': Any group name.
         'login': Required if True.
 
         Restrictions are defined as a list of dicts specifying rules for access. Rules are evaluated until one denies
         access or the list ends.
+
+        Rules are enforced by `crm.utils.check_permissions()`.
         """
         try:
             return settings.CRM_CONTENT_RESTRICTIONS
         except AttributeError:
             return self.restrictions
+
+    def _nav_menu(self, current_prefix) -> {str: tuple}:
+        """
+        Generates a complete navigation tree for the current content root and a sub-tree for the current prefix.
+        """
+        add_prefix = lambda prefix, node: "{prefix}{node}".format(prefix=prefix + '/' if prefix != '' else prefix,
+                                                                  node=node)
+        search_path = os.path.join(settings.BASE_DIR, 'templates/crm/' + self.get_content_root())
+        current_prefix = '/'.join(current_prefix.split('/')[:-1])
+        current_path = os.path.join(search_path, current_prefix)
+
+        def _generate_menu(base_path, prefix=''):
+            """"""
+            for node in os.listdir(base_path):
+                node_slug = re.sub('.html', '', node)
+                path = os.path.join(base_path, prefix, node)
+                if os.path.isdir(path):
+                    item = {'name': node,
+                            'isdir': True,
+                            'title': get_title(node_slug),
+                            'children': None,
+                            'slug': None}
+                    children = os.listdir(path)
+                    if children:
+                        item['children'] = tuple(_generate_menu(path, prefix=item['name']))
+                    if os.path.exists(os.path.join(path, 'index.html')):
+                        item['slug'] = add_prefix(prefix, node_slug)
+                    yield item
+                else:
+                    if prefix != '' and node_slug == 'index':
+                        continue
+                    yield {'name': add_prefix(prefix, node),
+                           'isdir': False,
+                           'title': get_title(node_slug),
+                           'children': None,
+                           'slug': add_prefix(prefix, node_slug)}
+        return {'all': tuple(_generate_menu(search_path)),
+                'current': tuple(_generate_menu(current_path, prefix=current_prefix))}
 
     def get(self, request, path=None):
         """
@@ -55,22 +99,17 @@ class Content(View):
         """
         content_root = self.get_content_root()
         restrictions = self.get_restrictions()
-        if not root_exists(content_root):
-            raise Http404
         try:
             check_permissions(path, restrictions, request)
         except NotAllowed:
-            return HttpResponseForbidden('You are not allowed to access this resource!')
+            raise PermissionDenied
         except LoginRequired:
             return redirect_to_login(request.path)
         else:
-            path = 'index' if path is None else path
-            path = path[:-1] if path[-1] == '/' else path
-            template_path = '/'.join([content_root, path + '.html'])
+            path = get_slug(path)
+            template_path = '/'.join(['crm', content_root, path + '.html'])
             try:
-                return render(request, template_path, {'title': get_title(path)})
+                return render(request, template_path, {'title': get_title(path), 'nav_menu': self._nav_menu(path)})
             except TemplateDoesNotExist as e:
                 raise Http404(e)
-        finally:
-            log.exception('An unexpected error occurred!')
 content = Content.as_view()
